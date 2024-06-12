44f3bb4abf2099094fd1b1b75442582586e429a5
Aurel Lossou <aurelth@icloud.com>
2024-06-12 08:47:08 -0300

    ConnectionString Added as well as Auto Mapper Mappings

---
+++ API/MyFinances.API/MyFinances.API/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyFinances.API.Models.Domain;
+using MyFinances.API.Models.DTOs;
+
+namespace MyFinances.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Expense, ExpenseDTO>().ReverseMap();
+        }
+    }
+}
--- API/MyFinances.API/MyFinances.API/Program.cs
+++ API/MyFinances.API/MyFinances.API/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyFinances.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(typeof(Program));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
