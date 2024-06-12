634d516c660f660c353c0bd7eb847a29fa11f387
Aurel Lossou <aurelth@icloud.com>
2024-06-12 08:30:52 -0300

    Criação do ExpenseDTO

--- API/MyFinances.API/MyFinances.API/Models/DTOs/ExpenseDTO.cs
+++ API/MyFinances.API/MyFinances.API/Models/DTOs/ExpenseDTO.cs
@@ -1,6 +1,6 @@
-﻿namespace MyFinances.API.Models
+﻿namespace MyFinances.API.Models.DTOs
 {
-    public class Expense
+    public class ExpenseDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ API/MyFinances.API/MyFinances.API/Models/Domain/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFinances.API.Models.Domain
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
