e4080aa27d2094a182095cad4d150f05b48ec7e4
Aurel Lossou <aurelth@icloud.com>
2024-06-12 08:33:01 -0300

    ApplicationDbContext.cs created

---
+++ API/MyFinances.API/MyFinances.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFinances.API.Models.Domain;
+
+namespace MyFinances.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
