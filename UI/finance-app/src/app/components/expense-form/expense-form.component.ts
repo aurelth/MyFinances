15411ce0f8dfd52aa751cc7389f07008820d929b
Aurel Lossou <aurelth@icloud.com>
2024-06-13 07:55:03 -0300

    expense-form.component.html and expense-form.component.ts implementations

--- UI/finance-app/src/app/components/expense-form/expense-form.component.ts
+++ UI/finance-app/src/app/components/expense-form/expense-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseService } from 'src/services/services/expense.service';
+import { Expense } from 'src/models/expense.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-expense-form',
   templateUrl: './expense-form.component.html',
-  styleUrls: ['./expense-form.component.scss']
+  styleUrls: ['./expense-form.component.scss'],
 })
 export class ExpenseFormComponent implements OnInit {
+  expense: Expense = { id: 0, name: '', amount: 0, date: new Date() };
 
-  constructor() { }
+  constructor(private expenseService: ExpenseService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.expense.id === 0) {
+      this.expenseService.addExpense(this.expense).subscribe(() => {
+        this.router.navigate(['/']);
+      });
+    } else {
+      this.expenseService.updateExpense(this.expense).subscribe(() => {
+        this.router.navigate(['/']);
+      });
+    }
   }
-
 }
