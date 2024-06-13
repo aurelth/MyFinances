8613f8de39cb2ce92c94c1119fdf45bc9e570270
Aurel Lossou <aurelth@icloud.com>
2024-06-13 06:57:57 -0300

    expense-list Template and Class implemented

--- UI/finance-app/src/app/components/expense-list/expense-list.component.ts
+++ UI/finance-app/src/app/components/expense-list/expense-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseService } from 'src/services/services/expense.service';
+import { Expense } from 'src/models/expense.model';
 
 @Component({
   selector: 'app-expense-list',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expense-list.component.scss']
 })
 export class ExpenseListComponent implements OnInit {
+  expenses: Expense[] = [];
+  filteredExpenses: Expense[] = [];
+  totalExpenses: number = 0;
+  selectedMonth: string = '';
+  months: string[] = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
-  constructor() { }
+  constructor(private expenseService: ExpenseService) { }
 
   ngOnInit(): void {
+    this.loadExpenses();
   }
 
+  loadExpenses(): void {
+    this.expenseService.getExpenses().subscribe(data => {
+      this.expenses = data;
+      this.filterByMonth();
+    });
+  }
+
+  filterByMonth(): void {
+    const monthIndex = this.months.indexOf(this.selectedMonth) + 1;
+    this.filteredExpenses = this.expenses.filter(expense =>
+      new Date(expense.date).getMonth() + 1 === monthIndex
+    );
+    this.calculateTotal();
+  }
+
+  calculateTotal(): void {
+    this.totalExpenses = this.filteredExpenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+
+  editExpense(expense: Expense): void {
+    // I will implement edit functionality soon
+  }
+
+  deleteExpense(id: number): void {
+    this.expenseService.deleteExpense(id).subscribe(() => {
+      this.loadExpenses();
+    });
+  }
 }
