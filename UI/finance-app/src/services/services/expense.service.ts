d417cd0c3e03a1fb047c791e9a8870b07a9c1996
Aurel Lossou <aurelth@icloud.com>
2024-06-13 06:52:57 -0300

    expense.service Created

--- UI/finance-app/src/services/services/expense.service.ts
+++ UI/finance-app/src/services/services/expense.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Expense } from 'src/models/expense.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExpenseService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getExpenses(): Observable<Expense[]> {
+    return this.http.get<Expense[]>(environment.apiUrl);
+  }
+
+  getExpense(id: number): Observable<Expense> {
+    return this.http.get<Expense>(`${environment.apiUrl}/${id}`);
+  }
+
+  addExpense(expense: Expense): Observable<Expense> {
+    return this.http.post<Expense>(environment.apiUrl, expense);
+  }
+
+  updateExpense(expense: Expense): Observable<void> {
+    return this.http.put<void>(`${environment.apiUrl}/${expense.id}`, expense);
+  }
+
+  deleteExpense(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrl}/${id}`);
+  }
 }
