0714c5ae5da3de799f6410d31a7710dfdc55f396
Aurel Lossou <aurelth@icloud.com>
2024-06-13 06:25:10 -0300

    Controller Methods implementation

--- API/MyFinances.API/MyFinances.API/Controllers/ExpensesController.cs
+++ API/MyFinances.API/MyFinances.API/Controllers/ExpensesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFinances.API.Models.DTOs;
+using MyFinances.API.Services.Interface;
 
 namespace MyFinances.API.Controllers
 {
@@ -7,5 +9,53 @@ namespace MyFinances.API.Controllers
     [ApiController]
     public class ExpensesController : ControllerBase
     {
+        private readonly IExpenseService _service;
+
+        public ExpensesController(IExpenseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseDTO>>> GetExpenses()
+        {
+            return Ok(await _service.GetExpensesAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseDTO>> GetExpense(int id)
+        {
+            var expense = await _service.GetExpenseByIdAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return Ok(expense);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddExpense(ExpenseDTO expenseDto)
+        {
+            await _service.AddExpenseAsync(expenseDto);
+            return CreatedAtAction(nameof(GetExpense), new { id = expenseDto.Id }, expenseDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpense(int id, ExpenseDTO expenseDto)
+        {
+            if (id != expenseDto.Id)
+            {
+                return BadRequest();
+            }
+            await _service.UpdateExpenseAsync(expenseDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(int id)
+        {
+            await _service.DeleteExpenseAsync(id);
+            return NoContent();
+        }
     }
 }
--- API/MyFinances.API/MyFinances.API/Program.cs
+++ API/MyFinances.API/MyFinances.API/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using MyFinances.API.Data;
+using MyFinances.API.Repositories.Implementation;
+using MyFinances.API.Repositories.Interface;
+using MyFinances.API.Services.Implementation;
+using MyFinances.API.Services.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IExpenseService, ExpenseService>();
 builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddEndpointsApiExplorer();
